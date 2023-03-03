d98e630f80e995a7e6939e3dcb368d3aa997463a
nat-davydova <natti.davydova@gmail.com>
2023-03-03 21:29:44 +0300

    feat: add sliding element

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,18 @@ import "virtual:svg-icons-register";
 
 const DOM = {
   timeline: "timeline",
+  timelineStepper: "timeline__stepper",
   timelineStep: "timeline__step",
   timelineStepActive: "is-active",
+  timelineSlideElement: "timeline__slide",
 };
 
 const timeline = document.querySelector(`.${DOM.timeline}`);
+const timelineStepper = document.querySelector(`.${DOM.timelineStepper}`);
 
-function deactivateSteps(): void {
-  const steps = document.querySelectorAll(`.${DOM.timelineStep}`);
-  steps?.forEach((elem) => elem.classList.remove(`${DOM.timelineStepActive}`));
-}
-
-function activateCurrentStep(currentStep: Element): void {
-  currentStep?.classList.add(`${DOM.timelineStepActive}`);
-}
+window.addEventListener("load", () => {
+  createSlideElement();
+});
 
 timeline?.addEventListener("click", (event) => {
   const { target } = event;
@@ -37,3 +35,18 @@ timeline?.addEventListener("click", (event) => {
   deactivateSteps();
   activateCurrentStep(currentStep);
 });
+
+function deactivateSteps(): void {
+  const steps = document.querySelectorAll(`.${DOM.timelineStep}`);
+  steps?.forEach((elem) => elem.classList.remove(`${DOM.timelineStepActive}`));
+}
+
+function activateCurrentStep(currentStep: Element): void {
+  currentStep?.classList.add(`${DOM.timelineStepActive}`);
+}
+
+function createSlideElement() {
+  const slideElement = document.createElement("div");
+  slideElement.classList.add(`${DOM.timelineSlideElement}`);
+  timelineStepper?.appendChild(slideElement);
+}
